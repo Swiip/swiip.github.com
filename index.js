e0a8134b48fa5d0bf05fc5443a9bb9c6305b389f
Swiip <matthieu.lux@gmail.com>
2012-08-20 14:15:41 +0200

    publications twitter

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 $(document).scroll(function(){
-    // If has not activated (has no attribute "data-top"
-    if (!$('.subnav').attr('data-top')) {
-        // If already fixed, then do nothing
-        if ($('.subnav').hasClass('subnav-fixed')) return;
-        // Remember top position
-        var offset = $('.subnav').offset()
-        $('.subnav').attr('data-top', offset.top);
-    }
+  // If has not activated (has no attribute "data-top"
+  if (!$('.subnav').attr('data-top')) {
+    // If already fixed, then do nothing
+    if ($('.subnav').hasClass('subnav-fixed')) return;
+    // Remember top position
+    var offset = $('.subnav').offset()
+    $('.subnav').attr('data-top', offset.top);
+  }
 
-    if ($('.subnav').attr('data-top') - $('.subnav').outerHeight() <= $(this).scrollTop())
-        $('.subnav').addClass('subnav-fixed');
-    else
-        $('.subnav').removeClass('subnav-fixed');
+  if ($('.subnav').attr('data-top') - $('.subnav').outerHeight() <= $(this).scrollTop())
+    $('.subnav').addClass('subnav-fixed');
+  else
+    $('.subnav').removeClass('subnav-fixed');
 });
 
-$.ready(function() {
-  $('.subnav').scrollspy()
+$(function() {
+  var twitterApiUrl = "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=Swiip&count=5&callback=?";
+  $.getJSON(twitterApiUrl, function(data) {
+    $(".tweet").html(_.template($("#twitter-template").html(), {data: data}));
+  });
 });
