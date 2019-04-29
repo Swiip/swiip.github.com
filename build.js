02792ee3d389420335a1cc0a2c65b6eac2250bd4
Swiip <matthieu.lux@gmail.com>
2019-04-29 10:17:17 +0200

    first test config using now

---
+++ build.js
@@ -0,0 +1,14 @@
+const {join} = require("path")
+const {ensureDir, copyFile} = require("fs-extra")
+
+const fileName = "Swiip.jpg"
+const distPath = join(__dirname, "dist")
+const sourcePath = join(__dirname, fileName)
+const destPath = join(distPath, fileName)
+
+const run = async () => {
+  await ensureDir(distPath)
+  await copyFile(sourcePath, destPath)
+}
+
+run();
