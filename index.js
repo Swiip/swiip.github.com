add9bccb2a5bcfd7b0948a27d04adbfc9f7d556c
Swiip <matthieu.lux@gmail.com>
2019-04-29 12:14:31 +0200

    add tooling

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express")
-const {join} = require("path")
+const express = require("express");
+const { join } = require("path");
 
 const app = express();
 
-const filePath = join(__dirname, "dist", "Swiip.jpg")
+const filePath = join(__dirname, "dist", "Swiip.jpg");
 
 app.use((req, res) => {
   res.sendFile(filePath);
-})
+});
 
 module.exports = app;
